db23780f9055557d7e280ae6f55a9318e0e47adb
Metalted <68029407+metalted@users.noreply.github.com>
2023-11-02 18:10:43 +0100

    Added control keys and autosend

---
+++ ConfigManagement.cs
@@ -0,0 +1,59 @@
+﻿using BepInEx.Configuration;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ChatUtilities
+{
+    public static class ConfigManagement
+    {
+        public static ConfigFile config;        
+        public static ConfigEntry<KeyCode> emoticonKey;
+        public static ConfigEntry<KeyCode> commandKey;
+        public static ConfigEntry<KeyCode> clipboardKey;
+        public static ConfigEntry<KeyCode> clearKey;
+        public static ConfigEntry<KeyCode> closeKey;
+        public static ConfigEntry<string> autoSendSuffix;
+
+        public static List<ConfigEntry<string>> commandConfigEntries = new List<ConfigEntry<string>>();
+        private static int commandCount = 9;
+
+        public static void Initialize(ConfigFile cfg)
+        {
+            config = cfg;
+
+            emoticonKey = config.Bind("Controls", "Emoticon Key", KeyCode.LeftControl, "");
+            commandKey = config.Bind("Controls", "Command Key", KeyCode.LeftAlt, "");
+            clipboardKey = config.Bind("Controls", "Clipboard Key", KeyCode.PageDown, "");
+            clearKey = config.Bind("Controls", "Clear Key", KeyCode.Delete, "");
+            closeKey = config.Bind("Controls", "Close Key", KeyCode.End, "");
+            autoSendSuffix = config.Bind("Preferences", "Auto Send Suffix", "<send>", "");
+
+            for(int i = 0; i < commandCount; i++)
+            {
+                commandConfigEntries.Add(config.Bind("Commands", "Command " + (i + 1), "", ""));
+            }
+        }
+
+        //Returns all commands that have any value assigned through the settings.
+        public static List<string> GetCommands()
+        {
+            List<string> commands = new List<string>();
+
+            for (int i = 0; i < commandCount; i++)
+            {
+                string key = "Command " + (i + 1);
+                try
+                {
+                    string configValue = (string)config["Commands", key].BoxedValue;
+                    if (!string.IsNullOrEmpty(configValue))
+                    {
+                        commands.Add(configValue);
+                    }
+                }
+                catch { }
+            }
+
+            return commands;
+        }
+    }
+}
---
+++ RectManagement.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChatUtilities
+{
+    public static class RectManagement
+    {
+        public static bool GetChatRect(out Rect chatRect)
+        {
+            chatRect = new Rect();
+
+            if (Plugin.onlineChatUI == null)
+            {                
+                return false;
+            }
+
+            try
+            {
+                RectTransform chatboxBackgroundImage = Plugin.onlineChatUI.bigChatInput.GetComponent<Image>().rectTransform;
+                Vector2 size = Vector2.Scale(chatboxBackgroundImage.rect.size, chatboxBackgroundImage.lossyScale);
+                Rect screenSpace = new Rect((Vector2)chatboxBackgroundImage.position - (size * 0.5f), size);
+                screenSpace.y = Screen.height - screenSpace.y - size.y;
+                chatRect = screenSpace;
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static Rect GetRectUnderChat(Rect chatRect)
+        {
+            return new Rect(chatRect.position.x, chatRect.position.y + chatRect.size.y, chatRect.size.x, Screen.height - chatRect.position.y - chatRect.size.y);
+        }
+
+        public static Rect GetRectRightOfChat(Rect chatRect, int rectIndex)
+        {
+            return new Rect(chatRect.position.x + chatRect.size.x + rectIndex * chatRect.size.y, chatRect.position.y, chatRect.size.y, chatRect.size.y);
+        }
+
+        public static List<Rect> SubdivideRect(Rect rect, int columns, int rows)
+        {
+            List<Rect> rects = new List<Rect>();
+
+            if(columns <= 0 || rows <= 0)
+            {
+                Debug.Log("Columns || Rows shouldnt be <= 0");
+                rects.Add(rect);
+                return rects;
+            }
+
+            float width = rect.width / columns;
+            float height = rect.height / rows;
+
+            for (int row = 0; row < rows; row++)
+            {
+                for(int col = 0; col < columns; col++)
+                {
+                    float x = rect.x + col * width;
+                    float y = rect.y + row * height;
+                    rects.Add(new Rect(x, y, width, height));
+                }
+            }
+
+            return rects;
+        }
+    }
+}
